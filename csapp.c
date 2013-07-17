7dd8dc359e3f00c16da3ff30dbdd1abbf50b19a8
OrangeQ <erhai_1987@126.com>
2013-07-17 10:55:01 +0800

    Create csapp.c

---
+++ csapp.c
@@ -0,0 +1,172 @@
+#include "csapp.h"
+
+ssize_t
+rio_readn(int fd, void *usrbuf, size_t n) {
+  size_t nleft = n;
+	ssize_t nread;
+	char *bufp = usrbuf;
+
+	while (nleft > 0) {
+		if ((nread = read(fd, bufp, nleft)) < 0) {
+			if (errno == EINTR)
+				nread = 0;
+			else
+				return -1;
+		} else if (nread == 0)
+			break;
+		nleft -= nread;
+		bufp += nread;
+	}
+
+	return n - nleft;
+}
+
+
+
+
+ssize_t 
+rio_writen(int fd, void *usrbuf, size_t n) {
+	size_t nleft = n;
+	ssize_t nwritten;
+	char *bufp = usrbuf;
+
+	while (nleft > 0) {
+		if ((nwritten = write(fd, bufp, nleft)) <= 0) {
+			if (errno == EINTR)
+				nwritten = 0;
+			else
+				return -1;
+		}
+		nleft -= nwritten;
+		bufp += nwritten;
+	}
+	return n;
+}
+
+int main(void)
+{
+	char a[MAXLINE];
+	int fd1, fd2;
+	ssize_t d1, d2;
+
+	fd1 = open("11.txt", O_RDONLY, 0);
+	fd2 = open("12.txt", O_RDWR | O_CREAT, 777);
+	d1 = rio_readn(fd1, a, 10);
+	d2 = rio_writen(fd2, a, 20);
+	close(fd1);
+	close(fd2);
+	printf("%d\n", (int)d1);
+	printf("%d\n", (int)d2);
+}
+/*
+void 
+rio_readinitb(rio_t *rp, int fd) {
+	rp->rio_fd = fd;
+	rp->rio_cnt = 0;
+	rp->rio_bufptr = rp->rio_buf;
+}
+
+
+
+static ssize_t 
+rio_read(rio_t *rp, char *usrbuf, size_t n) {
+	int cnt;
+
+	while (rp->rio_cnt <= 0) {
+		rp->rio_cnt = read(rp->rio_fd, rp->rio_buf,
+				sizeof(rp->rio_buf));
+		if (rp->rio_cnt < 0) {
+			if (errno != EINTR)
+				return -1;
+		} else if (rp->rio_cnt == 0)
+			return 0;
+		else
+			rp->rio_bufptr = rp->rio_buf;
+	}
+
+	cnt = n;
+	if (rp->rio_cnt < n)
+		cnt = rp->rio_cnt;
+	memcpy(usrbuf, rp->rio_bufptr, cnt);
+	rp->rio_bufptr += cnt;
+	rp->rio_cnt -= cnt;
+	return cnt;
+}
+
+ssize_t
+rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen) {
+	int n, rc;
+	char c, *bufp = usrbuf;
+
+	for (n = 1; n < maxlen; n++) {
+		if ((rc = rio_read(rp, &c, 1)) == 1) {
+			*bufp++ = c;
+			if (c == '\n')
+				break;
+		} else if (rc == 0) {
+			if (n == 1)
+				return 0;
+			else 
+				break;
+		} else
+			return -1;
+	}
+	*bufp = 0;
+	return n;
+}
+
+ssize_t
+rio_readnb(rio_t *rp, void *usrbuf, size_t n) {
+}
+
+int
+open_clientfd(char *hostname, int port)
+{
+	int clientfd;
+	struct hostent *hp;
+	struct sockaddr_in serveraddr;
+
+	if ((clientfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		return -1;
+
+	if ((hp = gethostbyname(hostname)) == NULL)
+		return -2;
+
+	bzero((char *)&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	bcopy((char *)hp->h_addr_list[0], (char *)&serveraddr.sin_addr.s_addr, hp->h_length);
+	serveraddr.sin_port = htons(port);
+
+	if (connect(clientfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
+		return -1;
+
+	return clientfd;
+}
+
+int
+open_listenfd(int port)
+{
+	int listenfd, optval = 1;
+	struct sockaddr_in serveraddr;
+
+	if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		return -1;
+
+	if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR,
+			(const void *)&optval, sizeof(int)) < 0)
+		return -1;
+
+	bzero((char *)&serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons((unsigned short)port);
+
+	if (bind(listenfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
+		return -1;
+
+	if (listen(listenfd, LISTENQ) < 0)
+		return -1;
+
+	return listenfd;
+}
+*/
